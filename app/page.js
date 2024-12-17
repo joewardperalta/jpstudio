d9902ddf84a6153b29c5f671d32cb6d244b31787
Joeward Peralta <joeward_peralta@outlook.com>
2024-12-17 14:58:03 -0500

    Create the hero section for the homepage which include the title, cta button, and product shot

--- app/page.js
+++ app/page.js
@@ -1,7 +1,32 @@
+import Image from "next/image";
+import Title from "@/components/Title";
+import Wrapper from "@/components/wrapper";
+import PrimaryButton from "@/components/buttons/PrimaryButton";
+
 export default function Home() {
   return (
     <main>
-      <p>Hello World!</p>
+      <section className="bg-black" id="hero-section">
+        <Wrapper>
+          <div className="max-w-[73.5rem] mx-auto flex flex-col items-center my-20">
+            <Title className="text-center text-white">
+              We build modern and professional website for your business
+            </Title>
+            <PrimaryButton className="bg-primary text-white">
+              Get Started
+            </PrimaryButton>
+          </div>
+          <div>
+            <Image
+              className="rounded-5xl"
+              src="/images/comfort-sofa.jpg"
+              width={3840}
+              height={2160}
+              alt="hero image of a sofa for a furniture E-commerce website"
+            />
+          </div>
+        </Wrapper>
+      </section>
     </main>
   );
 }
