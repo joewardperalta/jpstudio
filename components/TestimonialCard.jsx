100a07ade63c52ee0a853f777719c3c00b56cfb6
Joeward Peralta <joeward_peralta@outlook.com>
2025-01-23 16:47:23 -0500

    Add functionality to the read more button that expands the body text when it is clicked and unexpand when it is clicked the second time.

--- components/TestimonialCard.jsx
+++ components/TestimonialCard.jsx
@@ -1,9 +1,34 @@
+"use client";
 import Image from "next/image";
 import SecondaryButton from "./buttons/SecondaryButton";
+import { useRef } from "react";
 
 export default function TestimonialCard({ author, avatar, message }) {
+  const messageBoxRef = useRef(null);
+  const readMoreButtonRef = useRef(null);
+
+  // Expand the message box to read the full text
+  function expandMessage() {
+    // Expand/unexpand message box
+    messageBoxRef.current.classList.toggle("line-clamp-4");
+
+    // Get a reference of the secondary button body text element
+    let secondaryButtonTextElement = readMoreButtonRef.current
+      .querySelector(".secondary-button-body")
+      .querySelector("p");
+
+    // Change the button text to read more/read less
+    if (secondaryButtonTextElement.innerText == "Read more") {
+      console.log("Read more");
+      secondaryButtonTextElement.innerText = "Read less";
+    } else {
+      console.log("Read less");
+      secondaryButtonTextElement.innerText = "Read more";
+    }
+  }
+
   return (
-    <div className="rounded-[4rem] border border-lightGray py-30 px-17 min-w-[37.5rem] w-[37.5rem] inline-block ">
+    <div className="rounded-[4rem] border border-lightGray py-30 px-17 max-w-[37.5rem] w-full inline-block h-fit">
       <div className="flex gap-5 mb-[2.1875rem]">
         {/* Avatar */}
         <Image
@@ -24,9 +49,13 @@ export default function TestimonialCard({ author, avatar, message }) {
       </div>
 
       {/* Message */}
-      <p className="mb-[2.1875rem] line-clamp-4">{message}</p>
+      <p className="mb-[2.1875rem] line-clamp-4" ref={messageBoxRef}>
+        {message}
+      </p>
 
-      <SecondaryButton>Read more</SecondaryButton>
+      <SecondaryButton onClick={expandMessage} ref={readMoreButtonRef}>
+        Read more
+      </SecondaryButton>
     </div>
   );
 }
--- components/buttons/SecondaryButton.jsx
+++ components/buttons/SecondaryButton.jsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 
-export default function SecondaryButton({ children }) {
+export default function SecondaryButton({ children, onClick, ref }) {
   return (
-    <button className="text-[1rem] w-fit font-medium">
-      <div className="flex gap-x-4 items-center">
-        {children}
+    <button
+      className="text-[1rem] w-fit font-medium"
+      onClick={onClick}
+      ref={ref}
+    >
+      <div className="secondary-button-body flex gap-x-4 items-center">
+        <p>{children}</p>
         <Image
           src="/vectors/arrow-right-long.svg"
           width={25}
